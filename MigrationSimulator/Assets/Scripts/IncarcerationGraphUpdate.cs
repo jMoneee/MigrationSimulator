1541e43a183442fd682bcee70d3cd9995c56a56a
jMoneee <jdlarse1@asu.edu>
2020-04-04 15:08:21 -0700

    Proof of Concept graph

---
+++ MigrationSimulator/Assets/Scripts/IncarcerationGraphUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ChartAndGraph;
+
+public class IncarcerationGraphUpdate : MonoBehaviour {
+    
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        BarChart barChart = GetComponent<ChartAndGraph.BarChart>();
+        barChart.DataSource.SetValue("Number Incarcerated","Migrant Population", 50);
+        barChart.DataSource.SetValue("Number Incarcerated", "Native Population", 500);
+    }
+}
