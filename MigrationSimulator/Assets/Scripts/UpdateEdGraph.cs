99529648c6aef52fb2d6a18c2ec2535302159172
jMoneee <jdlarse1@asu.edu>
2020-04-05 12:07:09 -0700

    Added Graphs into Sample Scene with Graphs

---
+++ MigrationSimulator/Assets/Scripts/UpdateEdGraph.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ChartAndGraph;
+
+public class UpdateEdGraph : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        BarChart barChart = GetComponent<BarChart>();
+        //if (barChart != null) {
+        //barChart.DataSource.SlideValue( "Some HS", "Native Born", 20, 4f);
+        barChart.DataSource.SetValue( "Some HS", "Foreign Born", MigrationFramework.getSpecificPopValue("frHighschool")/1000);        barChart.DataSource.SetValue("HS Grad", "Foreign Born", MigrationFramework.getSpecificPopValue("frHighschoolGrad") / 1000);
+        barChart.DataSource.SetValue("Some College", "Foreign Born", MigrationFramework.getSpecificPopValue("frTwoYear") / 1000);
+        barChart.DataSource.SetValue("Bachelors", "Foreign Born", MigrationFramework.getSpecificPopValue("frBachelors") / 1000);
+        barChart.DataSource.SetValue("Other", "Foreign Born", MigrationFramework.getSpecificPopValue("frOther") / 1000);
+
+        barChart.DataSource.SetValue("Some HS", "Native Born", MigrationFramework.getSpecificPopValue("usHighschool") / 1000);        barChart.DataSource.SetValue("HS Grad", "Native Born", MigrationFramework.getSpecificPopValue("usHighschoolGrad") / 1000);
+        barChart.DataSource.SetValue("Some College", "Native Born", MigrationFramework.getSpecificPopValue("usTwoYear") / 1000);
+        barChart.DataSource.SetValue("Bachelors", "Native Born", MigrationFramework.getSpecificPopValue("usBachelors") / 1000);
+        barChart.DataSource.SetValue("Other", "Native Born", MigrationFramework.getSpecificPopValue("usOther") / 1000);
+        //}
+    }
+}
