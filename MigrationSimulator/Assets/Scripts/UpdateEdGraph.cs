13e2cc479caefb30d5b35eb0da34ad26ea14183d
jMoneee <jdlarse1@asu.edu>
2020-04-05 13:17:31 -0700

    Updated Education graph

--- MigrationSimulator/Assets/Scripts/UpdateEdGraph.cs
+++ MigrationSimulator/Assets/Scripts/UpdateEdGraph.cs
@@ -15,11 +15,13 @@ public class UpdateEdGraph : MonoBehaviour {
         BarChart barChart = GetComponent<BarChart>();
         //if (barChart != null) {
         //barChart.DataSource.SlideValue( "Some HS", "Native Born", 20, 4f);
+        barChart.DataSource.SetValue("Below HS", "Foreign Born", MigrationFramework.getSpecificPopValue("frLessThanHighschool") / 1000);
         barChart.DataSource.SetValue( "Some HS", "Foreign Born", MigrationFramework.getSpecificPopValue("frHighschool")/1000);        barChart.DataSource.SetValue("HS Grad", "Foreign Born", MigrationFramework.getSpecificPopValue("frHighschoolGrad") / 1000);
         barChart.DataSource.SetValue("Some College", "Foreign Born", MigrationFramework.getSpecificPopValue("frTwoYear") / 1000);
         barChart.DataSource.SetValue("Bachelors", "Foreign Born", MigrationFramework.getSpecificPopValue("frBachelors") / 1000);
         barChart.DataSource.SetValue("Other", "Foreign Born", MigrationFramework.getSpecificPopValue("frOther") / 1000);
 
+        barChart.DataSource.SetValue("Below HS", "Native Born", MigrationFramework.getSpecificPopValue("usLessThanHighschool") / 1000);
         barChart.DataSource.SetValue("Some HS", "Native Born", MigrationFramework.getSpecificPopValue("usHighschool") / 1000);        barChart.DataSource.SetValue("HS Grad", "Native Born", MigrationFramework.getSpecificPopValue("usHighschoolGrad") / 1000);
         barChart.DataSource.SetValue("Some College", "Native Born", MigrationFramework.getSpecificPopValue("usTwoYear") / 1000);
         barChart.DataSource.SetValue("Bachelors", "Native Born", MigrationFramework.getSpecificPopValue("usBachelors") / 1000);
