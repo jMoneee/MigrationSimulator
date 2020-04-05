8b475dd7774a58a9fcc327bd42ba29cfb8a9cc9a
jMoneee <jdlarse1@asu.edu>
2020-04-05 12:24:18 -0700

    Added Incarceration Graph

---
+++ MigrationSimulator/Assets/Scripts/UpdateIncGraph.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ChartAndGraph;
+
+
+public class UpdateIncGraph : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        BarChart barChart = GetComponent<BarChart>();
+        //if (barChart != null) {
+        //barChart.DataSource.SlideValue( "Some HS", "Native Born", 20, 4f);
+        barChart.DataSource.SetValue("Native Born", "All", MigrationFramework.getSpecificPopValue("usInc") / 1000);        barChart.DataSource.SetValue("Legal Migrant", "All", MigrationFramework.getSpecificPopValue("frLegalInc") / 1000);
+        barChart.DataSource.SetValue("Illegal Migrant", "All", MigrationFramework.getSpecificPopValue("frIllegalInc") / 1000);
+        
+        //}
+    }
+}
+
